96124005abdfc86389a6c56f37d60b0ec544d2e1
Sang <duongcongsang2000@gmail.com>
2023-11-22 14:53:41 +0700

    up

--- Read.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Mở file JSON để đọc
-with open('Data.json', 'r', encoding='utf-8') as file:
-    # Sử dụng json.load để đọc nội dung từ file và chuyển đổi thành Python data structure
-    data = json.load(file)
-
-# In ra nội dung đã đọc
-print(data)
